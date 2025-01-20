f60b1cb6c4456d193d829121e9e93f2087e560be
Eric Hannafin <erichannafin1990@gmail.com>
2025-01-20 21:22:49 +0000

    Adding drawCard method

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,8 +10,14 @@ let sumEl = document.getElementById("sum-el");
 let cardEl = document.getElementById("card-el")
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent += cards[i] + " "
+    }
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } else if (sum === 21) {
@@ -24,7 +31,16 @@ function startGame() {
 }
 
 function drawCard() {
+    console.log("Drawing a new card from the deck!")
+    let newCard = Math.floor((Math.random() * 10) + 1);
+    cards.push(newCard)
+    sum += newCard
+    cardEl.textContent = "Cards: "
+    renderGame()
+}
 
+function updateCards(newCard) {
+    cardEl.textContent += " " + newCard
 }
 
 document.getElementById("startButton").addEventListener("click", () => startGame());

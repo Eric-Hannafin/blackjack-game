159fdfe617358fd246de7d0c37c6ac1816dc6c40
Eric Hannafin <erichannafin1990@gmail.com>
2025-01-20 20:26:56 +0000

    Adding card id

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el")
 
 function startGame() {
     sumEl.textContent = "Sum: " + sum
+    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } else if (sum === 21) {
